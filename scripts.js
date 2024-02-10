d9078a44b5ffa147b9a7849d881f1944259eaccd
selipearantes <felipearantes.dev@gmail.com>
2024-02-10 17:52:47 -0300

    Treinado JavaScript - Treino simples coloquei email no input e aparece uma confirmação na tela

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function enviarParaConsole() {
+    // Obtenha o valor do campo de entrada
+    const texto = document.getElementById('inputText').value;
+
+    // Exiba o valor no console
+   alert("Email enviado com sucesso! " + texto);}
+   
