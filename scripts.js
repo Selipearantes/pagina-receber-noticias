29abb653810143d64ed032ff360cd35db5dcf321
selipearantes <felipearantes.dev@gmail.com>
2024-02-11 00:24:51 -0300

    Treino Js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 function enviarParaConsole() {
     // Obtenha o valor do campo de entrada
     const texto = document.getElementById('inputText').value;
+   if (null >= texto) { alert('Email não informado');
 
+}
+
+else  {alert("Email enviado com sucesso!");}}
     // Exiba o valor no console
-   alert("Email enviado com sucesso! " + texto);}
    
